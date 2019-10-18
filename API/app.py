723716ea4551bd0fd8ab59195e491d9d8ee08034
adjeielias90 <adjeielias90@gmail.com>
2019-10-18 13:50:15 +0000

    Server on port 5000

--- API/app.py
+++ API/app.py
@@ -14,3 +14,11 @@ FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 # * ---------- Create App --------- *
 app = Flask(__name__)
 CORS(app, support_credentials=True)
+
+
+# * -------------------- RUN SERVER -------------------- *
+if __name__ == '__main__':
+    # * --- DEBUG MODE: --- *
+    app.run(host='127.0.0.1', port=5000, debug=True)
+    #  * --- DOCKER PRODUCTION MODE: --- *
+    # app.run(host='0.0.0.0', port=os.environ['PORT']) -> DOCKER
